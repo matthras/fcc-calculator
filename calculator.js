b40cd9c0900bbe3b940bca8997ec414865960396
Matthew Mack <Matthew Mack>
2016-07-18 14:03:14 +1000

    sketched out JS functions

---
+++ calculator.js
@@ -0,0 +1,13 @@
+var displayArray = []; // Array of string?
+
+var pushone = function() {
+  // When a button is pressed, push the corresponding value/operation into displayArray
+
+  // Update the display
+}
+
+var evaluate = function() {
+  // Combine all elements in displayArray into a single expression and evaluate it
+
+  // Update the displays
+}
