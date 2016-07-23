d0f82d8e42c3ad52bed61abe88a8de069740980c
Matthew Mack <Matthew Mack>
2016-07-23 22:24:26 +1000

    added template code for displayUpdate

--- calculator.js
+++ calculator.js
@@ -4,10 +4,12 @@ var pushone = function() {
   // When a button is pressed, push the corresponding value/operation into displayArray
 
   // Update the display
+  document.getElementById('display').innerHTML()
 }
 
 var evaluate = function() {
   // Combine all elements in displayArray into a single expression and evaluate it
 
-  // Update the displays
+  // Update the
+  document.getElementById('display').innerHTML()
 }
